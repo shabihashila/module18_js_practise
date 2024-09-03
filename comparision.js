4d69b2c3242377700a20685b95dd5ac19529f9d5
HP <HP@DESKTOP-8E0FCDH>
2024-09-03 11:57:25 +0600

    comparision.js

---
+++ comparision.js
@@ -0,0 +1,32 @@
+// practise true false
+
+console.log(5 > 10);
+console.log(5 < 10);
+console.log(5 == 10);
+console.log(10 >= 10);
+console.log(10 <= 10);
+console.log(5 >= 10);
+// ..................................
+var priceAm = 30;
+var priceIce = 450;
+console.log(priceIce < priceAm);
+
+var priceAm = 30;
+var priceIce = 450;
+console.log(priceIce >= priceAm);
+
+var priceAm = 30;
+var priceIce = 450;
+console.log(priceIce <= priceAm);
+
+var priceAm = 30;
+var priceIce = 450;
+console.log(priceIce == priceAm);
+
+console.log(10 == 10);
+console.log(10 == "10");
+console.log(10 === "10");
+console.log(10 === "sweet");
+// exceptional case 😒😒😒
+console.log(10 != "10");
+console.log(10 !== "10");
---
+++ variable.js
@@ -0,0 +1,18 @@
+var price = 10;
+price += 20;
+console.log(price);
+
+/*
+* comparision
+1.bigger >
+2.less <
+3.equal ==
+4.>= greater than or equal
+5.<= less than or equal
+not euqal : !=
+6.and : &&
+both sorto mante hbe duita sorto dile
+7.or: ||
+any ekta sorto manlei hbe
+
+*/
